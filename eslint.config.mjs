7dcba93bc7f42586e40cce9ae63f6930fcd54aef
Shuvrojit Biswas <jsuvro17@gmail.com>
2024-09-16 21:18:56 +0600

    Setup development tools
    Tools: Typescript, eslint, prettier, husky

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+];
