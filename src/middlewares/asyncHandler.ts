3bcc3088d708850ef0ecfe0dc04be2c2989de2aa
Shuvrojit Biswas <jsuvro17@gmail.com>
2026-07-14 17:06:30 +0600

    fix(middleware): accept Express handler return types

--- src/middlewares/asyncHandler.ts
+++ src/middlewares/asyncHandler.ts
@@ -4,7 +4,7 @@ type AsyncRequestHandler = (
   req: Request,
   res: Response,
   next: NextFunction
-) => Promise<unknown>;
+) => void | Promise<unknown>;
 
 /**
  * Wrapper for async controllers to catch errors and pass them to the error handler middleware
